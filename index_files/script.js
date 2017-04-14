50f45ce154dc3e16264e880df6a586913c4eece5
Iacchus Mercurius <kassivs@gmail.com>
2017-04-14 03:12:50 -0300

    ok

--- index_files/script.js
+++ index_files/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   $('#main').load('fretboard.svg')
+
+  $("#chordname").change(function(){
+  	var chord_name = this.value()
+	var chord = teoria.chord(chord_name)
+
+	var notes = chord.notes()
+
+	for(note=0; notes[note]; note++)
+		console.log(notes[note]);
+  })
 })
