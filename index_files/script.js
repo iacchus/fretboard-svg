b4dc862efcd1d84ed9f5584b1b59dc86037f5e7d
Iacchus Mercurius <kassivs@gmail.com>
2017-04-14 04:03:46 -0300

    ok

--- index_files/script.js
+++ index_files/script.js
@@ -16,7 +16,22 @@ $(document).ready(function(){
 	        note_name = notes[note]
 		int_semitones = voicing[note].semitones()
 	  	data.push([note_name, int_semitones])
-	}	 
+	}	
+
+	//remove ex classes
+	//$()
+
+
+	for(note=0; data[note]; note++)
+	{
+		cell_class = ".note-cell-name-" + data[note][0]
+		circ_class = ".note-circ-name-" + data[note][0]
+		text_class = ".note-text-name-" + data[note][0]
+		$(cell_class).each(fuunction(){$(this).addClass("cell-selected cell-int-" + data[note][1])})
+		$(circ_class).each(fuunction(){$(this).addClass("circ-selected circ-int-" + data[note][1])})
+		$(text_class).each(fuunction(){$(this).addClass("text-selected text-int-" + data[note][1])})
+	}
+
 	console.log(data);
   })
 })
